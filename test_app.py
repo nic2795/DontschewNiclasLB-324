215333061887bb7c43c9599041e0834588544e5c
Niclas Dontschew <niclas@dontschew.com>
2026-09-20 18:26:46 +0200

    Endprodukt

--- test_app.py
+++ test_app.py
@@ -2,8 +2,6 @@ from app import app, entries
 
 import pytest
 
-# Use Flask's test client for testing
-
 
 @pytest.fixture()
 def client():
@@ -15,32 +13,27 @@ def client():
 
 
 def test_add_entry(client):
-    # Test adding an entry
+
     response = client.post(
         '/add_entry', data={'content': 'Test Entry Content'})
 
-    # Check if the response is a redirect to the index page
     assert response.status_code == 302
     assert response.headers['Location'] == '/'
 
-    # Check if the entry was added to the database
     entry = entries[0]
     assert entry is not None
     assert entry.content == 'Test Entry Content'
 
 
 def test_add_entry_with_happiness(client):
-    # Test adding an entry with happiness
+
     response = client.post(
         '/add_entry', data={'content': 'Test Entry Content',
                              'happiness': '😃'})
 
-    # Check if the response is a redirect to the index page
     assert response.status_code == 302
     assert response.headers['Location'] == '/'
 
-    # Check if the entry was added to the database with the correct
-    # happiness
     entry = entries[0]
     assert entry is not None
     assert entry.content == 'Test Entry Content'
